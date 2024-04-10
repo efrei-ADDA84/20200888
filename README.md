a62abe2447464baee4c8e80d42ce6b964edc1b6c
JZhg02 <94388221+JZhg02@users.noreply.github.com>
2024-04-10 16:33:59 +0200

    weather-app wrapper

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # 20200888
 Repo de Jacques ZHANG
+
+Wrapper renseignant la condition météorologique de la position donnée par les coordonnées (latitude, longitude) dans les variables d'environnement. <br/> Utilisation de l'API de OpenWeather. <br/> 
+
+Commandes utilisées pour la conteneurisation sur Docker: <br/>
+
+Création de l'image: 
+```bash
+docker build -t weatherapp .
+```
+Vérification de la création de l'image: 
+```bash
+docker images
+```
+Création et Run le container: 
+```bash
+docker run --name weather-app weatherapp
+```
+Simple Run: 
+```bash
+docker start -i weather-app
+```
---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+lat = os.getenv("LATITUDE")
+long = os.getenv("LONGITUDE")
+apikey = os.getenv("API_KEY")
+
+
+def make_api_call(latitude, longitude, api_key):
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}"
+    try:
+        response = requests.get(url)
+        response.raise_for_status() 
+        return response.json()
+    except requests.exceptions.HTTPError as err:
+        raise SystemExit(err)
+
+
+def extract_weather(data):
+    try:
+        weather = data['weather'][0]['main']
+        return weather
+    except KeyError as err:
+        raise SystemExit(err)
+
+
+def main():
+    data = make_api_call(lat, long, apikey)
+    weather = extract_weather(data)
+    print(f"The weather at position [{lat}, {long}] is {weather}")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
